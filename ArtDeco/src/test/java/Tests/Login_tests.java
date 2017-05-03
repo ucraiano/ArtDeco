a4cd2f8f55e7e6fada055f5596aab884b82357ed
ucraiano <ucraiano@gmai.com>
2017-05-03 11:59:36 +0300

    Extent Report added

--- ArtDeco/src/test/java/Tests/Login_tests.java
+++ ArtDeco/src/test/java/Tests/Login_tests.java
@@ -1,13 +1,19 @@
 package Tests;
 
 import Pages.*;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,7 +26,8 @@ SignIn obj_SignIn;
 SelectRole obj_Select_Role;
 SignUp obj_SignUp;
 Facebook obj_Facebook;
-
+ExtentReports extent;
+ExtentTest logger;
 
 
 @BeforeTest
@@ -31,11 +38,18 @@ Facebook obj_Facebook;
         obj_Facebook = new Facebook(driver);
         driver.get("https://www.artdecobeauty.com");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/LoginReport.html", true);
+        extent.addSystemInfo("Host Name","artdecobeauty.com");
+        extent.addSystemInfo("Environment","Selenium WebDriver > FireFox Driver");
+        extent.addSystemInfo("Developed by","Aleksandr Savchenko");
+        extent.loadConfig(new File(System.getProperty("user.dir")+"/extent-configs/login-extent-config.xml"));
+
 }
 
 
 @Test(priority = 1)
     public void test_empty_fields() throws InterruptedException {
+        logger = extent.startTest("test_empty_fields");
         obj_Main.setFirst_visit_alert_close();
         obj_Main.setWait();
         obj_Main.setAccount_button();
@@ -48,6 +62,7 @@ Facebook obj_Facebook;
 }
 @Test(priority = 2)
     public void test_not_email(){
+        logger = extent.startTest("test_not_email");
         obj_SignIn.setEmail_field("asdasd.com");
         Assert.assertTrue(obj_SignIn.is_email_label_present());
         obj_SignIn.setPassword_field("123");
@@ -58,6 +73,7 @@ Facebook obj_Facebook;
 }
 @Test(priority = 3)
     public void test_non_existing_email(){
+        logger = extent.startTest("test_non_existing_email");
         obj_SignIn.setEmail_field("123@gmail.com");
         obj_SignIn.setPassword_field("123123");
         obj_SignIn.setSign_IN_button();
@@ -68,6 +84,7 @@ Facebook obj_Facebook;
 
 @Test(priority =4)
     public void test_existing_email_and_non_existing_password(){
+        logger = extent.startTest("test_existing_email_and_non_existing_password");
         obj_SignIn.setEmail_field("ucraiano.test1@gmail.com");
         obj_SignIn.setPassword_field("123123123");
         obj_SignIn.setSign_IN_button();
@@ -77,14 +94,17 @@ Facebook obj_Facebook;
 
 @Test(priority = 5)
     public void test_correct_email_with_one_space_in_the_beginning(){
+        logger = extent.startTest("test_correct_email_with_one_space_in_the_beginning");
         obj_SignIn.setEmail_field(" ucraiano.test1@gmail.com");
         obj_SignIn.setPassword_field("REM4)nexuses");
         obj_SignIn.setSign_IN_button();
         Assert.assertTrue(obj_SignIn.is_it_signIN_page().toLowerCase().contains("sign in"));
         Assert.assertTrue(obj_SignIn.is_facebook_present().toLowerCase().contains("sign in with facebook"));
+
 }
 @Test(priority = 6)
     public void test_correct_email_with_one_space_in_the_end(){
+        logger = extent.startTest("test_correct_email_with_one_space_in_the_end");
         obj_SignIn.setEmail_field("ucraiano.test1@gmail.com ");
         obj_SignIn.setPassword_field("REM4)nexuses");
         obj_SignIn.setSign_IN_button();
@@ -94,6 +114,7 @@ Facebook obj_Facebook;
 
 @Test(priority = 7)
     public void password_with_one_space_in_the_beginning(){
+        logger = extent.startTest("password_with_one_space_in_the_beginning");
         obj_SignIn.setEmail_field("ucraiano.test1@gmail.com");
         obj_SignIn.setPassword_field(" REM4)nexuses");
         obj_SignIn.setSign_IN_button();
@@ -103,6 +124,7 @@ Facebook obj_Facebook;
 
 @Test(priority = 8)
     public void password_with_one_space_in_the_end(){
+        logger = extent.startTest("password_with_one_space_in_the_end");
         obj_SignIn.setEmail_field("ucraiano.test1@gmail.com");
         obj_SignIn.setPassword_field("REM4)nexuses ");
         obj_SignIn.setSign_IN_button();
@@ -111,16 +133,32 @@ Facebook obj_Facebook;
 }
 @Test(priority = 9)
     public void test_correct_email_and_password(){
-        obj_SignIn.setEmail_field("ucraiano.test1@gmail.com");
+        logger = extent.startTest("test_correct_email_and_password");
+        obj_SignIn.setEmail_field("test1@gmail.com"); // changed !!! ucraiano.test1@gmail.com
         obj_SignIn.setPassword_field("REM4)nexuses");
         obj_SignIn.setSign_IN_button();
         Assert.assertTrue(obj_Main.is_successfully_logIN_and_logOUT_present().toLowerCase().contains("logged in successfully"));
         obj_Main.setClose_successfully_login_alert();
         Assert.assertTrue(obj_Main.is_it_my_account_button().toLowerCase().contains("my account"));
 }
+@AfterMethod
+    public void getReportResults(ITestResult result){
+        if(result.getStatus() == ITestResult.FAILURE){
+            logger.log(LogStatus.FAIL, "Failed Test Case is : "+result.getName());
+            logger.log(LogStatus.FAIL, "Failed with next error : "+ result.getThrowable());
+          //  logger.log(LogStatus.INFO, "Snapshot below: " + logger.addScreenCapture("Users/savchenkoaleksandr/Documents/Саша/Фото/IMG_4283.jpg"));
+        }else if (result.getStatus() == ITestResult.SKIP){
+            logger.log(LogStatus.SKIP, "Skipped Test Case is :"+result.getName());
+        }else {
+            logger.log(LogStatus.PASS,"Test Passed");
+        }
+        extent.endTest(logger);
+}
 
-@AfterTest
+    @AfterTest
     public void set_down(){
+        extent.flush();
+        extent.close();
         driver.close();
         driver.quit();
 
--- ArtDeco/src/test/java/Tests/Search_tests.java
+++ ArtDeco/src/test/java/Tests/Search_tests.java
@@ -2,13 +2,19 @@ package Tests;
 
 import Pages.*;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 
@@ -20,6 +26,8 @@ public class Search_tests {
     WebDriver driver;
     Main obj_Main;
     Search_result obj_Search_result;
+    ExtentReports extent;
+    ExtentTest logger;
 
     @BeforeTest
     public void set_up(){
@@ -28,11 +36,16 @@ public class Search_tests {
         obj_Search_result = new Search_result(driver);
         driver.get("https://www.artdecobeauty.com/");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-
+        extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/SearchReport.html", true);
+        extent.addSystemInfo("Host Name","artdecobeauty.com");
+        extent.addSystemInfo("Environment","Selenium WebDriver > FireFox Driver");
+        extent.addSystemInfo("Developed by","Aleksandr Savchenko");
+        extent.loadConfig(new File(System.getProperty("user.dir")+"/extent-configs/search-extent-config.xml"));
     }
 
     @Test(priority = 1)
     public void test_correct_full_name() throws InterruptedException {
+        logger = extent.startTest("test_correct_full_name");
         obj_Main.setFirst_visit_alert_close();
         obj_Main.setWait();
         obj_Main.setSearch_open();
@@ -47,6 +60,7 @@ public class Search_tests {
 
     @Test(priority = 2)
     public void test_hint_presents_and_start_search_from_hint_in_main_page(){
+        logger = extent.startTest("test_hint_presents_and_start_search_from_hint_in_main_page");
         obj_Main.setHome();
         obj_Main.setSearch_open();
         obj_Main.setSearch_input("Sun Kissed Bronzing Set");
@@ -62,6 +76,7 @@ public class Search_tests {
     @Test(priority = 3)
     // Test doesn't work if focus is not on the browser window !!!!!
     public void test_previous_query_hint_presents_after_search_open(){
+        logger = extent.startTest("test_previous_query_hint_presents_after_search_open");
         obj_Main.setSearch_open();
         Assert.assertTrue(obj_Main.is_search_all_results_for_present().toLowerCase().contains("show all results for \"sun kissed bronzing set\""));
         obj_Main.setSearch_all_results_for();
@@ -70,11 +85,11 @@ public class Search_tests {
         Assert.assertTrue(obj_Search_result.is_result_number_present().toLowerCase().contains("1"));
         Assert.assertTrue(obj_Search_result.is_product_present_in_the_list().toLowerCase().contains("sun kissed bronzing set"));
         Assert.assertTrue(obj_Search_result.is_first_in_list().toLowerCase().contains("sun kissed bronzing set"));
-
     }
 
     @Test(priority = 4)
     public void test_part_name_query(){
+        logger = extent.startTest("test_part_name_query");
         obj_Main.setSearch_open();
         obj_Main.setSearch_input("bronzing set");
         obj_Main.setSearch_start();
@@ -87,6 +102,7 @@ public class Search_tests {
 
     @Test(priority = 5)
     public void test_open_close_search_input(){
+        logger = extent.startTest("test_open_close_search_input");
         obj_Main.setSearch_open();
         obj_Main.setSearch_input("sasa");
         Assert.assertTrue(obj_Main.is_search_all_results_for_present().toLowerCase().contains("show all results for \"sasa\""));
@@ -96,6 +112,7 @@ public class Search_tests {
 
     @Test(priority = 6)
     public void test_empty_query() throws InterruptedException {
+        logger = extent.startTest("test_empty_query");
         obj_Main.setWait();
         obj_Main.setSearch_open();
         obj_Main.setWait();
@@ -105,6 +122,7 @@ public class Search_tests {
 
     @Test(priority = 7)
     public void test_empty_query_from_main_page() throws InterruptedException {
+        logger = extent.startTest("test_empty_query_from_main_page");
         obj_Main.setHome();
         obj_Main.setSearch_open();
         obj_Main.setWait();
@@ -112,11 +130,25 @@ public class Search_tests {
         Assert.assertTrue(obj_Search_result.is_title_SEARCH_RESULTS_present().toLowerCase().contains("search results"));
         Assert.assertFalse(obj_Search_result.is_title_NOT_FOUND_present().toLowerCase().contains("no products found"));
     }
+
+    @AfterMethod
+    public void getReportResults(ITestResult result){
+        if(result.getStatus() == ITestResult.FAILURE){
+            logger.log(LogStatus.FAIL, "Failed Test Case is : "+result.getName());
+            logger.log(LogStatus.FAIL, "Failed with next error : "+ result.getThrowable());
+        }else if (result.getStatus() == ITestResult.SKIP){
+            logger.log(LogStatus.SKIP, "Skipped Test Case is :"+result.getName());
+        }else if (result.getStatus() == ITestResult.SUCCESS){
+            logger.log(LogStatus.PASS,"Test Passed");
+        }
+        extent.endTest(logger);
+    }
+
     @AfterTest
     public void set_down(){
+        extent.flush();
+        extent.close();
         driver.close();
         driver.quit();
     }
-
-
 }
